d0ec8eaf4999e61cb25ede75539bbae47ba55d61
aytee-80 <amogelangthompson@gmail.com>
2025-06-16 02:29:22 +0200

    Add project files

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify,render_template
+from flask_cors import CORS 
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/reverse', methods=['POST'])
+def sort_word():
+    data = request.get_json()
+    word = data.get('word', '').lower() 
+    
+    order = list(word)
+    order.sort()
+    sorted_word = ''.join(order)
+    return jsonify({'sorted_word': sorted_word})
+
+if __name__ == '__main__':
+    app.run(debug=True)
